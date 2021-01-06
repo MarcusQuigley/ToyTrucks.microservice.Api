75de0ad460798033f35ca9d71fdc39c875fc2f38
marcus quigley <marcusquigleygmail.com>
2021-01-06 12:41:01 -0500

    Entities, DBContext and repos defns

---
+++ HessTrucks/HessTrucks.Services.TruckCatalog/DbContexts/TruckCatalogDbContext.cs
@@ -0,0 +1,24 @@
+﻿using HessTrucks.Services.TruckCatalog.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HessTrucks.Services.TruckCatalog.DbContexts
+{
+    public class TruckCatalogDbContext : DbContext
+    {
+        public TruckCatalogDbContext(DbContextOptions<TruckCatalogDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Truck> Trucks { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ HessTrucks/HessTrucks.Services.TruckCatalog/Repositories/ITruckRepository.cs
@@ -0,0 +1,14 @@
+﻿using HessTrucks.Services.TruckCatalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HessTrucks.Services.TruckCatalog.Repositories
+{
+    public interface ITruckRepository
+    {
+        Task<IEnumerable<Truck>> GetTrucks();
+        Task<Truck> GetTruckById(Guid truckId);
+    }
+}
