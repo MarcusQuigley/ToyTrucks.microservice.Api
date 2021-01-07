e53478dd64cc4b3e6e45cfd07a6beb785c4e8f87
marcus quigley <marcusquigleygmail.com>
2021-01-07 12:47:31 -0500

    adding Repositories

---
+++ HessTrucks/HessTrucks.Services.TruckCatalog/Repositories/PhotoRepository.cs
@@ -0,0 +1,27 @@
+﻿using HessTrucks.Services.TruckCatalog.DbContexts;
+using HessTrucks.Services.TruckCatalog.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HessTrucks.Services.TruckCatalog.Repositories
+{
+    public class PhotoRepository : IPhotoRepository
+    {
+        private readonly TruckCatalogDbContext _context;
+
+        public PhotoRepository(TruckCatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Photo>> GetPhotosById(Guid truckId)
+        {
+            return await _context.Photos
+                                .Where(p => p.TruckId == truckId)
+                                .ToListAsync();
+        }
+    }
+}
---
+++ HessTrucks/HessTrucks.Services.TruckCatalog/Repositories/TruckRepository.cs
@@ -0,0 +1,37 @@
+﻿using HessTrucks.Services.TruckCatalog.DbContexts;
+using HessTrucks.Services.TruckCatalog.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HessTrucks.Services.TruckCatalog.Repositories
+{
+    public class TruckRepository : ITruckRepository
+    {
+        private readonly TruckCatalogDbContext _context;
+
+        public TruckRepository(TruckCatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Truck> GetTruckById(Guid truckId)
+        {
+            var truck = await _context.Trucks
+                                  .Include(t=>t.Photos)
+                                  .Where(t => t.TruckId == truckId)
+                                  .FirstOrDefaultAsync();
+            return truck;                                       
+        }
+
+        public async Task<IEnumerable<Truck>> GetTrucks()
+        {
+            return await _context.Trucks
+                                    .Include(t => t.Photos)
+                                    .ToListAsync();
+
+        }
+    }
+}
