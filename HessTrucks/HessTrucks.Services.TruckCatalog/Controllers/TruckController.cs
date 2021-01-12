dc726ae3671f6002cca6f578be5515e005b71ae5
marcus quigley <marcusquigleygmail.com>
2021-01-12 11:25:45 -0500

    inital Truck controller

---
+++ HessTrucks/HessTrucks.Services.TruckCatalog/Controllers/TruckController.cs
@@ -0,0 +1,42 @@
+﻿using HessTrucks.Services.TruckCatalog.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HessTrucks.Services.TruckCatalog.Controllers
+{
+    [Route("api/Truck")]
+    [ApiController]
+    public class TruckController : ControllerBase
+    {
+        private readonly ITruckRepository _truckService;
+
+        public TruckController(ITruckRepository truckService)
+        {
+            _truckService = truckService;
+        }
+
+        [HttpGet]
+        [Route("{categoryId:int}")]
+        public async Task<ActionResult> TrucksByCategoryId(int categoryId)
+        {
+            var trucks = await _truckService.GetTrucksByCategoryId(categoryId);
+            return Ok(trucks);
+
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Trucks()
+        {
+            return Ok(await _truckService.GetTrucks( ));
+
+
+        }
+    }
+
+
+}
